424801a28662aa80225a9f22fbd93eda51f5772b
guillermo rodriguez <guillermordgz.96@gmail.com>
2022-12-15 20:50:11 -0800

    question test

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const commenceButton = document.getElementById('start-btn')
+const coverElement = document.getElementById('question-cover')
+const questionElement = document.getElementById('question')
+const choicesElement = document.getElementById('choices')
+
+let randomQuestions, currentQuestions
 
 commenceButton.addEventListener('click', commenceGame)
 
 function commenceGame() {
-    
+    commenceButton.classList.add('hide')
+    randomQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestions = 0
+    coverElement.classList.remove('hide')
+    nextQuestion()
 }
 
 function nextQuestion() {
+    showQuestion(randomQuestions[currentQuestions])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach (answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectResponse)
+        choicesElement.appendChild(button)
+    })
 }
 
 function selectResponse() {
@@ -16,6 +39,11 @@ function selectResponse() {
 
 const questions = [
     {
-
+        question: 'gamora?',
+        answers: [
+            { text: 'who', correct: false},
+            { text: 'where', correct: false},
+            { text: 'why', correct: true}
+        ]
     }
 ]
