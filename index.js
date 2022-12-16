a0667f6efe84ea7548e4cdefb4f80586def3caa4
guillermo rodriguez <guillermordgz.96@gmail.com>
2022-12-15 20:00:00 -0800

    js structure

---
+++ index.js
@@ -0,0 +1,21 @@
+const commenceButton = document.getElementById('start-btn')
+
+commenceButton.addEventListener('click', commenceGame)
+
+function commenceGame() {
+    
+}
+
+function nextQuestion() {
+
+}
+
+function selectResponse() {
+
+}
+
+const questions = [
+    {
+
+    }
+]
