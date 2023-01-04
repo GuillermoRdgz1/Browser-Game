23dfe3c31d69f09619388a06688139999dfa229e
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-01-04 15:05:46 -0800

    add questions

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const choicesElement = document.getElementById('choices')
 
 let randomQuestions, currentQuestions
 
+
 commenceButton.addEventListener('click', commenceGame)
 nextButton.addEventListener('click', () => {
     currentQuestions++
@@ -32,9 +33,9 @@ function showQuestion(question) {
         const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
+        // if (answer.correct) {
+        //     button.dataset.correct = answer.correct
+        // }
         button.addEventListener('click', selectResponse)
         choicesElement.appendChild(button)
     })
@@ -91,7 +92,7 @@ const questions = [
         ]
     },
     {
-        question: 'In "John Carpenters The Thing", what game is MacReady playing against a computer, near the beginning of the movie?',
+        question: 'In "John Carpenter\'s The Thing", what game is MacReady playing against a computer, near the beginning of the movie?',
         answers: [
             { text: 'Battleship', correct: false},
             { text: 'Chess', correct: true},
@@ -117,5 +118,181 @@ const questions = [
             { text: 'Scary Movie', correct: false}
         ]
     },
+    {
+        question: 'What classic horror movie was originally titled The Babysitter Murders?',
+        answers: [
+            { text: 'Rosemary\'s Baby', correct: false},
+            { text: 'Halloween', correct: true},
+            { text: 'The Cabin in the Woods', correct: false},
+            { text: 'Child\'s Play', correct: false}
+        ]
+    },
+    {
+        question: 'Who is the only character to appear in and survive all four "Jaws" movies?',
+        answers: [
+            { text: 'Sean Brody', correct: false},
+            { text: 'Milton Brody', correct: false},
+            { text: 'Jason Brody', correct: false},
+            { text: 'Michael Brody', correct: true}
+        ]
+    },
+    {
+        question: 'Where does "Friday the 13th" take place?',
+        answers: [
+            { text: 'Camp Crystal Lake', correct: true},
+            { text: 'Camp Firewood', correct: false},
+            { text: 'Camp Victory Lake', correct: false},
+            { text: 'Camp Arawak', correct: false}
+        ]
+    },
+    {
+        question: 'What was the first horror film nominated for a Best Picture Oscar?',
+        answers: [
+            { text: 'The Shining', correct: false},
+            { text: 'The Evil Dead', correct: false},
+            { text: 'The Exorcist', correct: true},
+            { text: 'Final Destination', correct: false}
+        ]
+    },
+    {
+        question: 'What is the name of the parody movie series set within the “Scream” movies?',
+        answers: [
+            { text: 'Scream', correct: false},
+            { text: 'Ghostface', correct: false},
+            { text: 'Stab', correct: true},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: 'How many characters does Jason kill in the first “Friday the 13th" movie?',
+        answers: [
+            { text: '0', correct: true},
+            { text: '4', correct: false},
+            { text: '7', correct: false},
+            { text: '3', correct: false}
+        ]
+    },
+    {
+        question: 'How long does a character have to live after watching the haunted videotape in “The Ring”?',
+        answers: [
+            { text: '9 days', correct: false},
+            { text: '7 days', correct: true},
+            { text: '30 days', correct: false},
+            { text: '14 days', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
+    {
+        question: ' “Do you like scary movies?” is well known quote due to which film?',
+        answers: [
+            { text: 'Scream', correct: true},
+            { text: 'Halloween', correct: false},
+            { text: 'A Nightmare on Elm Street', correct: false},
+            { text: 'Scary Movie', correct: false}
+        ]
+    },
 ]
+
+var correct = document.getElementsByClassName("correct");
+var wrong = document.getElementsByClassName("wrong");
+var score = document.getElementById("score");
+
+
+var points = 0;
+
+choicesElement.addEventListener("click", function() {
+  
+  points += 1;
+  showPoints();
+});
+
+choicesElement.addEventListener("click", function() {
+
+  points -= 0;
+  showPoints();
+});
+
+function showPoints() {
+  score.innerHTML = points;
+}
 
